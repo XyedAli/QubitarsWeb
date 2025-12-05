d4d12256930c4ec8ed14a3f30bb7afd32f5666dd
Jawad Akram <ashah0515@gmail.com>
2025-12-05 21:28:29 +0500

    9:28

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,14 +1,3 @@
-// Export all types from home
-export type {
-  HeroIconSource,
-  HeroIconConfig,
-  Logo,
-  LogoGrid,
-  ChooseUsCard,
-  ChooseUsContent,
-  ExpertService,
-} from "./home";
-
 // Export all types from navbar
 export type { NavItem } from "./navbar";
 
