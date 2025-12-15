ccfa9ed4b70e3f38fd9dbd21f76e4796077f6ed8
Jawad Akram <ashah0515@gmail.com>
2025-12-15 22:03:38 +0500

    10:09

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import Hero from "@/components/modules/Hero";
-import AreaExpert from "@/components/modules/AreaExpert";
-import OurPartners from "@/components/modules/OurPartners";
-import ChooseUs from "@/components/modules/chooseUs";
-import HeroBg2 from "@/components/modules/herobg2";
-import HeroBg from "@/components/modules/herobg";
-import HeroBg3 from "@/components/modules/herobg3";
-import HeroBackground from "@/components/modules/herobg";
-export default function Home() {
-  return (
-    // <>
-    // <HeroBg2 />
-    // <HeroBg />
-
-    //   <Hero />
-    //   <AreaExpert />
-    //   <OurPartners />
-    //   <ChooseUs />
-    // </>
-    <main className="w-full overflow-x-hidden">
-    {/* <HeroBackground /> */}
-    {/* <HeroBg2 /> */}
-    <HeroBg3 />
-
-  </main>
-=======
 import { Hero } from "@/components/features/home";
 
 export default function Home() {
@@ -32,6 +5,5 @@ export default function Home() {
     <>
       <Hero />
     </>
->>>>>>> Hammad
   );
 }
