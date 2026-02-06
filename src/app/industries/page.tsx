d2b9f1de30706c44d2cebaf28087b66729cb57ad
faraway-yachting <ashah0515@gmail.com>
2026-02-06 15:11:40 +0500

    06-02-26

---
+++ src/app/industries/page.tsx
@@ -0,0 +1,36 @@
+import { Fragment } from "react";
+import {
+  IndustriesHero,
+  OtherSuccessStoriesSlider,
+  ChooseQubitars,
+  BusinessExcellence,
+  IndustriesAbout,
+  FAQs,
+} from "@/components/features/industries";
+import { OurClient } from "@/components/features/home";
+import { testimonialsData, testimonialImagesById } from "@/data/home";
+
+export const metadata = {
+  title: "Real Estate | Qubitars Technologies",
+  description:
+    "PropTech and real estate software solutions. Streamlined transactions, seamless experiences for buyers, investors, and tenants.",
+};
+
+export default function RealStatePage() {
+  const testimonialsWithImages = testimonialsData.map((testimonial) => {
+    const images = testimonialImagesById[testimonial.id];
+    return { ...testimonial, ...images };
+  });
+
+  return (
+    <Fragment>
+      <IndustriesHero />
+      <OtherSuccessStoriesSlider />
+      <ChooseQubitars />
+      <BusinessExcellence />
+      <IndustriesAbout />
+      <FAQs />
+      <OurClient testimonials={testimonialsWithImages} />
+    </Fragment>
+  );
+}
